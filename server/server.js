d5216bced6851baa45704bf643cf0eb9900d20e2
Aadi Si <aadisi1907@gmail.com>
2025-09-08 04:21:57 +0530

    fixed the cors issue

--- server/server.js
+++ server/server.js
@@ -1,25 +1,28 @@
-const dotenv = require("dotenv")
-dotenv.config({path:"./.env"})
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
 const express = require("express");
 const connectDB = require("./config/mongodb");
-const cors = require("cors")
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
-const app = express()
-connectDB()
-const port = process.env.PORT
-const allowedOrigins = ['http://localhost:5173']
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({origin:allowedOrigins,credentials : true}))
+const app = express();
+connectDB();
+const port = process.env.PORT;
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://store-it-frontend.onrender.com",
+];
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 //API Endpoint
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
-app.use("/auth",authRouter)
-app.use("/user",userRouter)
-app.listen(port,()=>{
-    console.log("Server is running")
-})
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
+app.listen(port, () => {
+  console.log("Server is running");
+});
